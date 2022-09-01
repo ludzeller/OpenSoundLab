71287d866f66aaebefa546638677a476b83ff6fd
Ludwig Zeller <ludwig.zeller@fhnw.ch>
2022-09-01 17:45:24 +0200

    first steps towards a more responsive glow on buttons

--- Assets/Scripts/UI/button.cs
+++ Assets/Scripts/UI/button.cs
@@ -55,24 +55,27 @@ public class button : manipObject {
   public override void Awake() {
     base.Awake();
     toggleKey = false;
-    glowColor = Color.HSVToRGB(glowHue, .5f, .25f);
+    
 
     if (_componentInterface == null) {
       if (transform.parent) _componentInterface = transform.parent.GetComponent<componentInterface>();
     }
+
+    glowColor = Color.HSVToRGB(glowHue, .75f, 1f);
 
     rend = GetComponent<Renderer>();
     offMat = rend.material;
     //offColor = offMat.GetColor("_Color");
     glowMat = new Material(onMat);
     glowMat.SetFloat("_EmissionGain", glowEmission);
-    glowMat.SetColor("_TintColor", glowColor);
-    selectOverlay.SetActive(false);
+    glowMat.SetColor("_TintColor", glowColor);    
 
     if (changeOverlayGlow) {
-      selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", glowColor);
-      selectOverlay.GetComponent<Renderer>().material.SetFloat("_EmissionGain", glowEmission);
+      selectOverlay.GetComponent<Renderer>().material.SetColor("_BaseColor", glowColor);
+      //selectOverlay.GetComponent<Renderer>().material.SetFloat("_EmissionGain", glowEmission);
     }
+
+    selectOverlay.SetActive(false);
 
     if (GetComponentInChildren<TextMesh>() != null) labelRend = GetComponentInChildren<TextMesh>().transform.GetComponent<Renderer>();
     if (labelRend != null) {
@@ -153,14 +156,14 @@ public class button : manipObject {
 
       if (glowMatOnToggle) rend.material = glowMat;
       if (labelRend != null) labelRend.material.SetFloat("_EmissionGain", labelEmission);
-      selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(1f, 1f, 1f));
+      //selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(1f, 1f, 1f));
     } else {
       if (singleID) _componentInterface.hit(on, buttonID);
       else _componentInterface.hit(on, button2DID[0], button2DID[1]);
 
       if (glowMatOnToggle) rend.material = offMat;
       if (labelRend != null) labelRend.material.SetFloat("_EmissionGain", .1f);
-      selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(0.5f, 0.5f, 0.5f));
+      //selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(0.5f, 0.5f, 0.5f));
     }
 
   }
@@ -175,23 +178,23 @@ public class button : manipObject {
       }
     }
     curState = state;
-    if (curState == manipState.none) {
+    if (curState == manipState.none && !toggled) {
       if (!singleID) _componentInterface.onSelect(false, button2DID[0], button2DID[1]);      
       selectOverlay.SetActive(false);
-      selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(0.5f, 0.5f, 0.5f));
+      //selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(0.5f, 0.5f, 0.5f));
     } else if (curState == manipState.selected) {
       if (!singleID) _componentInterface.onSelect(true, button2DID[0], button2DID[1]);
-      selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(0.7f, 0.7f, 0.7f));
+      //selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(0.7f, 0.7f, 0.7f));
       selectOverlay.SetActive(true);
     } else if (curState == manipState.grabbed) {
-      selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(1f, 1f, 1f));
+      //selectOverlay.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(1f, 1f, 1f));
       if (!singleID) _componentInterface.onSelect(true, button2DID[0], button2DID[1]);
       if (isToggle) {
         toggled = !toggled;
         if (toggled) keyHit(true);
         else if (!onlyOn) keyHit(false);
       } else keyHit(true);
-
+      selectOverlay.SetActive(true);
       if (!glowMatOnToggle) {
         rend.material = glowMat;
       }
@@ -215,6 +218,8 @@ public class button : manipObject {
             if (toggled) keyHit(true);
             else if (!onlyOn) keyHit(false);
           } else keyHit(true);
+
+          selectOverlay.SetActive(true);
 
           if (!glowMatOnToggle) {
             rend.material = glowMat;
